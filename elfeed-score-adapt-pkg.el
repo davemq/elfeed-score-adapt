938b56f861cd0a92f0225c98705898d4402b915a
Dave Marquardt <davemarq@linux.ibm.com>
2026-01-30 16:54:58 -0600

    More info

--- elfeed-score-adapt-pkg.el
+++ elfeed-score-adapt-pkg.el
@@ -1,4 +1,4 @@
-(define-package "elfeed-score-adapt" "0.8"
+(define-package "elfeed-score-adapt" "0.9"
   "Update elfeed score rules based on usage"
   '((elfeed-score "1.2.7")
     (elfeed "3.3.0")
